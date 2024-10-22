e579e7d875e0de8fa8afef568a9ad122058579d0
Curtis Cannon <curtis.cannon@hotmail.com>
2024-10-22 08:57:41 +0100

    Update Intune - Install Printer.ps1

--- Intune Scripts/Network Printer Deployment/Intune - Install Printer.ps1
+++ Intune Scripts/Network Printer Deployment/Intune - Install Printer.ps1
@@ -1,23 +1,37 @@
-﻿#Adjustable variables
+#Adjustable variables
 $DriverName = "Canon Generic Plus PCL6" #The Name of the printer driver listed in the INF file
 $PrinterName = "Canon Test Printer" #The name that you want assigned to the created printer and port
 $PrinterIP = "10.0.0.10" #The IP address of the printer in question
 $INFSource = $PSScriptRoot + "\x64\Driver\CNP60MA64.INF" #The Location of the INF file for your printer drivers (leave PSScriptRoot as it is)
 
+#Start logging actions
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\$($PrinterName) Deployment.log" -Force -Verbose
+
 #Add printer to driver store and collect published name
 $PNPOutput = pnputil -a $INFSource| Select-String "Published Name"
 $null = $pnpOutput -match "Published name :\s*(?<name>.*\.inf)" 
 $driverINF = Get-ChildItem -Path C:\Windows\INF\$($matches.Name)
+Write-Output "Driver added to driver store successfully"
 
 #Add printer driver from driver store
 Add-PrinterDriver -Name $DriverName -InfPath $driverinf.FullName
+Write-Output "Driver added to printer drivers successfully"
 
 #Add Printer port using information provided
 If ((Get-PrinterPort -Name $PrinterName -ErrorAction SilentlyContinue) -eq $null){
     Add-PrinterPort -Name $PrinterName -PrinterHostAddress $PrinterIP
+    Write-Output "Printer Port created successfully"
+}Else{
+    Write-Output "Printer Port already exists"
 }
 
 #Create Printer object using new driver and port, with provided details
 If ((Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue) -eq $null){
     Add-Printer -DriverName $DriverName -Name $PrinterName -PortName $PrinterName
+    Write-Output "Printer Created Successfully"
+}Else{
+    Write-Output "Printer already exists"
 }
+
+#Stop Logging
+Stop-Transcript
